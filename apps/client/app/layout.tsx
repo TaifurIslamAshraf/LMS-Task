584f70fa882d3ea9ffb7c74d42f493aefb55f561
TaifurIslamAshraf <Taifurislamashraf@gmail.com>
2025-09-02 08:23:01 +0600

    feat: product setup admin, client, server with turborepo

--- apps/client/app/layout.tsx
+++ apps/client/app/layout.tsx
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google"
 
 import "@workspace/ui/globals.css"
 import { Providers } from "@/components/providers"
+import { Metadata } from "next"
 
 const fontSans = Geist({
   subsets: ["latin"],
@@ -12,6 +13,12 @@ const fontMono = Geist_Mono({
   subsets: ["latin"],
   variable: "--font-mono",
 })
+
+export const metadata: Metadata = {
+  title: "LMS",
+  description: "this is an LMS Application",
+};
+
 
 export default function RootLayout({
   children,
